881537bacd87adc2a14c4838042e932d5a138ad2
vlad bakumenko <tinyyy11@yandex.ru>
2022-10-23 18:45:07 +0300

    start react app

---
+++ src/components/app/App.styles.ts
@@ -0,0 +1,18 @@
+import { createStyles } from "../../types/emotion-styles";
+
+export const styles = createStyles({
+   box: {
+      display: "flex",
+      alignItems: "center",
+      flexDirection: "column",
+      maxWidth: "1280px",
+      fontFamily: ' "Inter", Arial, sans-serif',
+      boxSizing: "border-box",
+      WebkitFontSmoothing: "antialiased",
+      MozOsxFontSmoothing: "grayscale",
+      WebkitTextSizeAdjust: "100%",
+      MozTextSizeAdjust: "100%",
+      minHeight: "100vh",
+      margin: "0 auto",
+   },
+});
---
+++ src/components/app/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.styles.ts";
+import { styles} from "../../templates/default/TemplateName.styles";
+
+function App() {
+   return <div css={styles.box}>ggg</div>;
+}
+
+export default App;
