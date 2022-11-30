6d741749c3df77f85ac9aa1a47b8596bfea27cd3
vlad bakumenko <tinyyy11@yandex.ru>
2022-11-30 16:31:05 +0300

    add custom hook

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,19 +1,44 @@
 import React, { FC } from "react";
 import { LoginProps } from "./types";
 import { styles } from "./Login.styles";
-import Input from '../../ui-kit/Input/Input';
+import Input from "../../ui-kit/Input/Input";
+import { useInput } from "../../ui-kit/Input/Input";
 
 const Login: FC<LoginProps> = () => {
+   const email = useInput("", { isEmpty: true, minLength: 3 });
+   const password = useInput("", { isEmpty: true, minLength: 5 });
    return (
       <div css={styles.box}>
          <h2 css={styles.title}>Рады вдеть</h2>
          <form css={styles.inputForm}>
-            <Input labels="email" name="email" maxLength={40} minLength={2}/>
-            <Input labels="password" name='password' maxLength={40} minLength={6}/>
-            <button
-            type="submit"
-            css={styles.submitButton}
-            >Войти</button>
+            {email.isDirty && email.isEmpty && (
+               <div style={{ color: "red" }}>Ошибка валидации</div>
+            )}
+            <Input
+               onChange={(e) => email.onChange(e)}
+               onBlur={(e) => email.onBlur(e)}
+               value={email.value}
+               labels="email"
+               name="email"
+               maxLength={40}
+               minLength={3}
+            />
+            {password.isDirty && password.isEmpty && (
+               <div style={{ color: "red" }}>Ошибка валидации</div>
+            )}
+
+            <Input
+               value={password.value}
+               onChange={(e) => password.onChange(e)}
+               onBlur={(e) => password.onBlur(e)}
+               labels="password"
+               name="password"
+               maxLength={40}
+               minLength={6}
+            />
+            <button type="submit" css={styles.submitButton}>
+               Войти
+            </button>
          </form>
       </div>
    );
--- src/ui-kit/Input/Input.tsx
+++ src/ui-kit/Input/Input.tsx
@@ -1,6 +1,53 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { styles } from "./Input.styles";
 import { InputProps } from "./Input.types";
+
+const useValidation = (value: string, validations: any) => {
+   const [isEmpty, setEmpty] = useState<boolean>(true);
+   const [minLengthError, setMinLengthError] = useState<boolean>(false);
+
+   useEffect(() => {
+      for (const validation in validations) {
+         switch (validation) {
+            case "minLength":
+               value.length < validations[validation]
+                  ? setMinLengthError(true)
+                  : setMinLengthError(false);
+               break;
+            case "isEmpty":
+               value ? setEmpty(false) : setEmpty(true);
+               break;
+         }
+      }
+   }, [value]);
+   return {
+      isEmpty,
+      minLengthError,
+   };
+};
+
+export const useInput = (initialValue: string , validations: any) => {
+   const [value, setValue] = useState(initialValue);
+   const [isDirty, setDirty] = useState<boolean>(false);
+
+   const valid = useValidation(value, validations);
+
+   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+   };
+
+   const onBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setDirty(true);
+   };
+
+   return {
+      value,
+      onChange,
+      onBlur,
+      isDirty,
+      ...valid
+   };
+};
 
 const Input: FC<InputProps> = ({
    value,
@@ -10,11 +57,13 @@ const Input: FC<InputProps> = ({
    onFocus,
    maxLength,
    minLength,
+   onBlur,
 }) => {
    return (
       <section className="InputSection">
          <div css={styles.box}>
             <label css={styles.inputLabel}>{labels}</label>
+
             <input
                css={styles.input}
                value={value}
@@ -24,6 +73,7 @@ const Input: FC<InputProps> = ({
                onFocus={onFocus}
                maxLength={maxLength}
                minLength={minLength}
+               onBlur={onBlur}
             />
          </div>
       </section>
